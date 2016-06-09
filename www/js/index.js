1d068279103c1701ae048818977aed802ec1aecc
Adriano Waltrick <akuma.majunia@gmail.com>
2016-06-09 12:47:34 -0300

    Teste para comlombia

--- www/js/index.js
+++ www/js/index.js
@@ -26,6 +26,38 @@ angular.module(
     function(
         $scope
     ) {
-        $scope.nome = "adriano";
+
+        $scope.converter = function()
+        {
+            var nr_qtd_taxa = $scope.nr_qtd_taxa;
+            var nr_qtd_peso = $scope.nr_qtd_peso;
+            var ds_conversao = $scope.ds_conversao;
+
+            nr_qtd_taxa = nr_qtd_taxa.replace(",", ".");
+            nr_qtd_peso = nr_qtd_peso.replace(",", ".");
+
+            var ds_valor =  nr_qtd_peso / nr_qtd_taxa;
+
+            if (ds_conversao == '*') {
+                var ds_valor =  nr_qtd_peso * nr_qtd_taxa;
+            }
+
+            $scope.nr_qtd_valor = ds_valor.toFixed(2).replace(
+               /./g,
+               function(c, i, a) {
+                  return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
+               }
+            );
+        }
+
+        $scope.taxaPeso = function()
+        {
+            $scope.nr_qtd_taxa = '0,0003443';
+        }
+
+        $scope.taxaDola = function()
+        {
+            $scope.nr_qtd_taxa = '2951,0500';
+        }
     }
 );
